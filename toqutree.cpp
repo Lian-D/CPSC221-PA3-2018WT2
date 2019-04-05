f8f8dce86f0dc84b150bd01a5e9251e903807e04
Lian <Lian.Duan@alumni.ubc.ca>
2019-04-04 21:17:30 -0700

    Update toqutree.cpp

--- toqutree.cpp
+++ toqutree.cpp
@@ -51,7 +51,7 @@ int upright = center + pow(2,k)/2;
 // Change the pixel by using getPixel, and then change the pixel 
 for(int x = 0; x < pow(2,k); x++){
 	for(int y = 0; y < pow(2,k); y++){
-		HSLAPixel* pixel = *subimage.getPixel(x,y);
+		HSLAPixel* pixel = subimage.getPixel(x,y);
 		HSLAPixel* originalpixel = imIn.getPixel(upleft + x, upleft + y);
 		*pixel = *originalpixel; 
 	}
